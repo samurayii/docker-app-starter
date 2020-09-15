a72e1a3ece703e0350c23e30e3be6b7b30286b27
Alexandr <samurayiii1986@gmail.com>
2020-09-15 16:57:44 +0300

    0.1.3

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 import config from "./lib/entry";
+import { Logger } from "logger-flx";
+import { Starter } from "./lib/starter";
 
-console.log(JSON.stringify(config, null, 4));
+const logger = new Logger(config.logger);
+const starter = new Starter(config.app, logger);
 
+starter.run();
+
+process.on("SIGTERM", () => {
+    console.log("💀 Termination signal received 💀");
+    process.exit();
+});
